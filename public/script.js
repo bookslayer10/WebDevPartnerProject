7d4d3044541b3736499bd358b1c336d3e41daba6
bookslayer10 <93895819+bookslayer10@users.noreply.github.com>
2023-11-23 11:22:41 -0800

    added database connection

--- public/script.js
+++ public/script.js
@@ -1,1 +1,26 @@
-// Write your JS in here
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+ 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js"
+
+/////////////////////////////////
+/// GLOBAL VARS
+/////////////////////////////////
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBlJCQXZh1S3fpxZqzGnp8VnG-04MO-O7M",
+    authDomain: "web-dev-partner-project.firebaseapp.com",
+    databaseURL: "https://web-dev-partner-project-default-rtdb.firebaseio.com",
+    projectId: "web-dev-partner-project",
+    storageBucket: "web-dev-partner-project.appspot.com",
+    messagingSenderId: "556366486052",
+    appId: "1:556366486052:web:860a1f7da246a91499d6b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+

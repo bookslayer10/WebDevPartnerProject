326039e85580fe5f777c81ba86b886402265f32b
bookslayer10 <93895819+bookslayer10@users.noreply.github.com>
2023-11-30 10:08:47 -0800

    added color updater and pass-through

--- public/script.js
+++ public/script.js
@@ -4,9 +4,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebas
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js"
-let id = 1; // id number for the idividual hexagons
 let hexDiv; //variable to create hexs
 
+// hex array
+let hexes = new Array();
+hexes.push(null);
+let isBoardLoaded = false;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -24,22 +27,22 @@ const firebaseConfig = {
 // Declaration of hex
 class Hex {
 
-
-  constructor(element, color) {
-    this.element = element;
-    this.setColor(color);
+  constructor(id, color) {
+    this.id = id;
+    this.color = color;
+    this.element = null;
   }
   
-  setColor(color){
-    console.log("setting color");
-    this.color = color;
-    this.element.style.backgroundColor = color;
+  assignElement(){
+    this.element = document.getElementById(this.id);
+  }
+  
+  updateColor(){
+    console.log("updatecolor" + this.color)
+    this.element.style.backgroundColor = this.color;
   }
 
 }
-
-// hex array
-let hexes = [null];
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -66,14 +69,23 @@ onValue(numberOfPlayersRef, (data) => {
 }); // onValue numPlayers
 
 onValue(hexesRef, (data) => {
-  hexes = data.val();
   
-  if(playerID == null && numberOfPlayers < 7){
-      numberOfPlayers++;
-      playerID = numberOfPlayers;
-
-      set(numberOfPlayersRef, numberOfPlayers);
+  if(data.val() == null){
+    createHexArray();
+    set(hexesRef, hexes);
+    console.log("createarray:");
+    console.log(hexes)
+  } else {
+    hexes = new Array();
+    hexes.push("test");   
+    for(let i = 1; i < data.val().length; i++){
+      hexes.push(new Hex(data.val()[i].id, data.val()[i].color));
+    }
+    console.log("getarray");
+    console.log(hexes);
   }
+
+  if(isBoardLoaded) updateGameBoard();
   
 }); // onValue numPlayers
 
@@ -93,11 +105,14 @@ window.onunload = (event) => {
 //AUTOMATE THE CREATION OF DIVS IN THE CONTAINER DIVS (CREATE FUNCTION).
 window.onload = function(){
 
+  let id = 1;
+
   for(let k = 1; k < 13; k++){
     let container = document.getElementById("c" + (k));
 	  container.setAttribute("class","container");
     for(let i = 1; i < 12 + k; i++){
-      createHex(container);
+      createHexElement(container, id);
+      id++;
     }
   } 
 
@@ -105,24 +120,56 @@ window.onload = function(){
     let container = document.getElementById("c" + (k + 12));
     container.setAttribute("class","container");
     for(let i = 1; i < 24 - k; i++){
-      createHex(container);
+      createHexElement(container, id);
+      id++;
     }
-  } 
+  }
+
+  console.log("window.onload finished")
+
+  isBoardLoaded = true;
+  updateGameBoard();
+
+  // 397 hexagon elements created
 }
 
-function createHex(container){
+function createHexElement(container, id){
   hexDiv = document.createElement("div"); 
   hexDiv.setAttribute("id", id);
   hexDiv.addEventListener("click", changeHexColor);
   container.appendChild(hexDiv);
+}
 
-  hexes.push(new Hex(hexDiv, "#444444"));
-  id++;
+function createHexArray(){
+  console.log("create hex array");
+  hexes = new Array();
+  hexes.push(null);
+  for(let i = 1; i < 398; i++){ // 397 elements from 1 to 397 inclusive
+    hexes.push(new Hex(i + "", "#009900"));
+  }
+
+  console.log(hexes);
 }
 
 const changeHexColor = (e) => {
+  console.log(hexes);
+  console.log(hexes[e.target.id]);
 
-  console.log(hexes[e.target.id])
+  hexes[e.target.id].assignElement();
+  console.log(hexes[e.target.id].element)
 
-  hexes[e.target.id].setColor("#990000");
+  hexes[e.target.id].color = "#990000";
+
+  set(hexesRef, hexes);
+}
+
+function updateGameBoard(){
+  console.log("update");
+  console.log(hexes.length);
+  console.log(hexes)
+  for(let i = 1; i < hexes.length; i++){
+    hexes[i].assignElement();
+    console.log(hexes[i]);
+    hexes[i].updateColor();
+  }
 }
